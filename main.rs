0f47bcc0049975a854f9f3a92ae64c10deb3f211
nicholasdejong <nroydejong@gmail.com>
2025-04-15 15:16:20 +0200

    Temporary main file

--- main.rs
+++ main.rs
@@ -1,13 +1,23 @@
-mod regex;
-use regex::*;
+mod lexer;
+mod matcher;
+mod parser;
+use lexer::*;
+use matcher::find_matches;
+use parser::*;
 use std::io::stdin;
 
 fn main() {
     let mut buf = String::new();
     loop {
+        println!("Haystack: ");
         stdin().read_line(&mut buf).expect("Failed to read line");
-        let tokens = lex_str(&buf.trim_end());
-        println!("{tokens:?}");
+        let haystack = buf.clone();
         buf.clear();
+        println!("Regex: ");
+        stdin().read_line(&mut buf).expect("Failed to read line");
+        let regex = buf.clone();
+        buf.clear();
+        println!("{haystack}, {regex}");
+        println!("{:?}", find_matches(&haystack.trim(), &regex.trim()));
     }
 }
