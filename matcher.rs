2024a2cb389b008d6e9b5cbdd65f317f69498da5
nicholasdejong <nroydejong@gmail.com>
2025-04-15 15:14:10 +0200

    Matcher exposes API

---
+++ matcher.rs
@@ -0,0 +1,27 @@
+// Returns all matches with a regex
+use crate::{lex_str, parse_tokens};
+
+pub fn find_matches(haystack: &str, regex: &str) -> Vec<usize> {
+    let tokens = lex_str(regex);
+    let root = parse_tokens(tokens);
+    let mut matches: Vec<usize> = Vec::new();
+    let mut i = 0;
+    'outer: while i < haystack.len() {
+        // println!("iter");
+        let mut j = 0;
+        while j < root.len() {
+            if i + j >= haystack.len() {
+                i += 1;
+                continue 'outer;
+            }
+            if !root[j].matches(haystack.chars().nth(i + j).unwrap() as u8) {
+                i += 1;
+                continue 'outer;
+            }
+            j += 1;
+        }
+        matches.push(i);
+        i += 1;
+    }
+    matches
+}
